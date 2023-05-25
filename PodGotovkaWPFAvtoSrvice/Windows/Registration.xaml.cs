35df11100229064e7b89ada12af4165fea33301b
romagor1 <mr.gorshkov2003@mail.ru>
2023-05-25 09:43:45 +0400

    Добавил регистрацию

--- PodGotovkaWPFAvtoSrvice/Windows/Registration.xaml.cs
+++ PodGotovkaWPFAvtoSrvice/Windows/Registration.xaml.cs
@@ -19,9 +19,25 @@ namespace PodGotovkaWPFAvtoSrvice.Windows
 	/// </summary>
 	public partial class Registration : Window
 	{
+		public static test_tbEntities _context = new test_tbEntities();
 		public Registration()
 		{
 			InitializeComponent();
+		}
+
+		private void AddNewUser_Click(object sender, RoutedEventArgs e)
+		{
+			_context.User.ToList();
+
+			_context.User.Add(new User
+			{
+				Name = TxtNameUser.Text,
+				Login = TxtLoginUser.Text,
+				Paswrord = TxtPasswordUser.Text,
+				Role = "user",
+			});
+			_context.SaveChanges();
+			this.Close();
 		}
 	}
 }
